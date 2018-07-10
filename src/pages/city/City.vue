509aa6b8fa99c8c7ea8486184f818a65ab09ff14
kingan <1017386624@qq.com>
2018-07-10 20:44:16 +0800

    添加城市页面搜索组件

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,16 +1,19 @@
 <template>
 	<div>
 		<city-header></city-header>
+		<city-search></city-search>
 	</div>
 </template>
 
 <script>
 import CityHeader from "./components/CityHeader"
+import CitySearch from "./components/CitySearch"
 
 export default {
 	name:'City',
 	components:{
 		CityHeader,
+		CitySearch
 	}
 }
 </script>
---
+++ src/pages/city/components/CitySearch.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="city-search">
+		<input class="city-search_search" type="text" placeholder="输入城市名或拼音" />
+	</div>
+</template>
+
+<script>
+export default {
+	name:'CitySearch',
+}
+</script>
+
+<style>
+	.city-search{
+		line-height:.72rem;
+		background-color: #00bcd4;
+		padding: 0 .2rem .1rem .2rem;
+	}
+	.city-search_search{
+		/*使区域不溢出*/
+		box-sizing: border-box;
+		width: 100%;
+		height: .62rem;
+		line-height: .62rem;
+		text-align: center;
+		padding: 0 .1rem;
+		border-radius: .06rem;
+	}
+
+</style>
